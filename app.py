90b42eed2f747169c629c553aefb4018401b776e
sofiamb31 <sofiamadrigal2303@gmail.com>
2025-05-01 14:02:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ with st.sidebar:
 st.markdown("""
     <style>
         .stApp {
-            background-color: FF007F 
+            background-color: FF007F ;
         }
     </style>
 """, unsafe_allow_html=True)
